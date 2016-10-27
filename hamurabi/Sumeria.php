e7fdc6151bcf4663093ac53d701ac6e7c2bb8506
Petros <p.morris@adfa.edu.au>
2016-10-27 13:40:03 +1100

    initialises reign

--- hamurabi/Sumeria.php
+++ hamurabi/Sumeria.php
@@ -1,3 +1,19 @@
 <?php namespace Petrosgames\hamurabi;
 
-class Sumeria
+class Sumeria {
+  public $current_year = 0;
+  public $reign = 10;
+  public $population = 0;
+  public $acres_owned = 0;
+  public $acres_planted = 0;
+  public $grain_in_silos = 0;
+  
+  function __construct() {
+    $this->population = 95;
+    $this->acres_owned = 1000;
+    $this->grain_in_silos = 2800; 
+  }
+  
+  
+  
+}
---
+++ hamurabi/index.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Hamurabi</title>
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+  <h1>Hamurabi</h1>  
+<p>Try your hand at governing ancient Sumeria successfully for a ten year term of office.</p>
+    <hr>
+
+  </body>
+</html>
